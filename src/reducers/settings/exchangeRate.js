deb01bc94fa73b0be5ad635faeaef1effcbff9b1
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-06-12 18:20:26 +0900

    Save default settings in initial save

--- src/reducers/settings/exchangeRate.js
+++ src/reducers/settings/exchangeRate.js
@@ -2,8 +2,9 @@ import {
   LOAD_SETTINGS_SUCCESS,
   UPDATE_EXCHANGE_RATE_SUCCESS
 } from '../../actions/settings'
+import { DEFAULT_BASE_CURRENCY } from '../../constants/currency'
 
-export default function(state = { USD: 1.0 }, action) {
+export default function(state = { [DEFAULT_BASE_CURRENCY]: 1.0 }, action) {
   switch (action.type) {
     case LOAD_SETTINGS_SUCCESS:
       if (!action.settings) return state
--- src/util/storage/settings.js
+++ src/util/storage/settings.js
@@ -1,4 +1,5 @@
 import Storage from './pouchdb'
+import { DEFAULT_BASE_CURRENCY } from '../../constants/currency'
 
 export async function retrieveSettings() {
   return Storage.settingsDB().get('settings').catch(err => {
@@ -13,6 +14,11 @@ export async function persistSettings(settings) {
     .then(doc => Storage.settingsDB().put({ ...doc, ...settings }))
     .catch(err => {
       if (err.status !== 404) throw err
-      return Storage.settingsDB().put({ _id: 'settings', ...settings })
+      return Storage.settingsDB().put({
+        _id: 'settings',
+        currency: { base: DEFAULT_BASE_CURRENCY, secondary: [] },
+        exchangeRate: { [DEFAULT_BASE_CURRENCY]: 1 },
+        ...settings
+      })
     })
 }
