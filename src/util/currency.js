d12a8d03c6f4e98cfadbb353336852bc9431ca93
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-06-14 18:42:15 +0900

    Refactor currency exchange update

--- src/util/currency.js
+++ src/util/currency.js
@@ -2,27 +2,24 @@ import fetch from 'isomorphic-fetch'
 
 export async function fetchExchangeRates(base, target) {
   const params = {
-    env: 'store://datatables.org/alltableswithkeys',
     q: buildQuery(base, target),
+    env: 'store://datatables.org/alltableswithkeys',
     format: 'json'
   }
 
   return fetch(`https://query.yahooapis.com/v1/public/yql?${encode(params)}`)
     .then(body => body.json())
-    .then(response => {
-      const exchangeRate = {}
-      for (let item of response.query.results.rate){
-        const code = item['Name'].split('/')[1]
-        exchangeRate[code] = parseFloat(item['Rate'])
-      }
-
-      return exchangeRate
-    })
+    .then(response =>
+      response.query.results.rate.reduce((result, row) => {
+        result[row.id.substring(3)] = parseFloat(row['Rate'])
+        return result
+      }, {})
+    )
 }
 
 function buildQuery(base, target) {
   const pairs = target.map(code => `${base}${code}`)
-  return `SELECT Name,Rate FROM yahoo.finance.xchange WHERE pair = "${pairs.join(',')}"`
+  return `SELECT id,Rate FROM yahoo.finance.xchange WHERE pair = "${pairs.join(',')}"`
 }
 
 function encode(params) {
