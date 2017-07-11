57cb24ac78707ed701718d2f2786b0535bd93677
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-07-11 11:57:08 +0900

    Add new routes

---
+++ src/containers/Budget.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../components/Header/index'
+import { Icon, Segment } from 'semantic-ui-react'
+
+class Budget extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header label="Budget" />
+        <Segment basic padded textAlign="center">
+          <Icon name="hourglass half" size="massive" />
+          <h3>Work in progress</h3>
+        </Segment>
+      </div>
+    )
+  }
+}
+
+export default Budget
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,10 @@
 import Dashboard from '../containers/Dashboard'
 import Transactions from '../containers/Transactions'
 import Accounts from '../containers/Accounts'
+import Budget from '../containers/Budget'
 import Reports from '../containers/Reports'
 import Settings from '../containers/Settings'
+import User from '../containers/User/index'
 
 export default [
   {
@@ -27,6 +29,13 @@ export default [
     component: Accounts
   },
   {
+    path: '/budget',
+    exact: false,
+    label: 'Budget',
+    icon: 'shopping basket',
+    component: Budget
+  },
+  {
     path: '/reports',
     exact: false,
     label: 'Reports',
@@ -39,5 +48,12 @@ export default [
     label: 'Settings',
     icon: 'options',
     component: Settings
+  },
+  {
+    path: '/user',
+    exact: true,
+    label: 'User',
+    icon: 'vcard outline',
+    component: User
   }
 ]
