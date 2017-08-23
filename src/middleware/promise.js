de4ed6ead1d14bc1ca32e3c7b59d6cd713624abd
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-08-23 16:34:12 +0900

    Dispatch separate actions for each stage of promisified action

--- src/middleware/promise.js
+++ src/middleware/promise.js
@@ -1,13 +1,19 @@
 const isPromise = val => val && typeof val.then === 'function'
 
+export const REQUEST = 'REQUEST'
+export const SUCCESS = 'SUCCESS'
+export const FAILURE = 'FAILURE'
+
 export default store => next => action => {
   if (!action.payload || !isPromise(action.payload)) return next(action)
 
+  const dispatch = store.dispatch
   const type = action.type
   const meta = action.meta || {}
-  store.dispatch({ type, meta, pending: true })
+
+  dispatch({ type: `${type}_${REQUEST}`, meta })
 
   return action.payload
-    .then(result => store.dispatch({ type, payload: result, complete: true }))
-    .catch(error => store.dispatch({ type, error, meta }))
+    .then(payload => dispatch({ type: `${type}_${SUCCESS}`, meta, payload }))
+    .catch(error => dispatch({ type: `${type}_${FAILURE}`, meta, error }))
 }
