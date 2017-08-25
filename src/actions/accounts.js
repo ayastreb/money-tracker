4b7860e2abff5cf7bfdf7d0c78112adbc7b3461e
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-08-25 22:32:14 +0900

    Refactor accounts actions

--- src/actions/accounts.js
+++ src/actions/accounts.js
@@ -1,19 +1,66 @@
 import { createActions } from 'redux-actions'
+import { showError } from './ui/error'
+import { setPendingChangesFlag } from './sync'
 import accounts from '../util/storage/accounts'
 
 export const {
-  loadAccounts,
-  syncAccounts,
-  saveAccount,
-  removeAccount,
-  changeBalance
-} = createActions({
-  LOAD_ACCOUNTS: accounts.loadAll,
-  SYNC_ACCOUNTS: accounts.sync,
-  SAVE_ACCOUNT: [account => accounts.save(account), account => ({ account })],
-  REMOVE_ACCOUNT: [id => accounts.remove(id), id => ({ id })],
-  CHANGE_BALANCE: [
-    (id, currency, amount) => accounts.changeBalance(id, currency, amount),
-    (id, currency, amount) => ({ id, currency, amount })
-  ]
-})
+  loadAccountsSuccess,
+  saveAccountRequest,
+  saveAccountFailure,
+  removeAccountRequest,
+  changeBalanceRequest
+} = createActions(
+  'LOAD_ACCOUNTS_SUCCESS',
+  'SAVE_ACCOUNT_REQUEST',
+  'SAVE_ACCOUNT_FAILURE',
+  'REMOVE_ACCOUNT_REQUEST',
+  'CHANGE_BALANCE_REQUEST'
+)
+
+export function syncAccounts() {
+  return dispatch => accounts.sync().then(() => dispatch(loadAccounts()))
+}
+
+export function loadAccounts() {
+  return dispatch => {
+    return accounts
+      .loadAll()
+      .then(accounts => dispatch(loadAccountsSuccess(accounts)))
+      .catch(error => dispatch(showError(error)))
+  }
+}
+
+export function saveAccount(account) {
+  return dispatch => {
+    dispatch(saveAccountRequest(account))
+
+    return accounts
+      .save(account)
+      .then(() => dispatch(setPendingChangesFlag()))
+      .catch(error => {
+        dispatch(saveAccountFailure(account.id))
+        dispatch(showError(error))
+      })
+  }
+}
+
+export function removeAccount(id) {
+  return dispatch => {
+    dispatch(removeAccountRequest(id))
+
+    return accounts
+      .remove(id)
+      .then(() => dispatch(setPendingChangesFlag()))
+      .catch(error => dispatch(showError(error)))
+  }
+}
+
+export function changeBalance(id, currency, amount) {
+  return dispatch => {
+    dispatch(changeBalanceRequest(id, currency, amount))
+
+    return accounts
+      .changeBalance(id, currency, amount)
+      .catch(error => dispatch(showError(error)))
+  }
+}
