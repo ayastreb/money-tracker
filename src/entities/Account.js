b0b68e3462d60e6b09205260b1ab9732d41535c8
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-09-01 16:01:53 +0900

    Implement account entity

---
+++ src/entities/Account.js
@@ -0,0 +1,64 @@
+import Currency from './Currency'
+
+const GROUP = {
+  cash: 'Cash',
+  bank: 'Bank Account',
+  deposit: 'Deposit',
+  credit: 'Credit',
+  asset: 'Asset'
+}
+
+export default {
+  defaultGroup: 'cash',
+  groupOptions() {
+    return Object.keys(GROUP).map(key => ({
+      key,
+      value: key,
+      text: GROUP[key]
+    }))
+  },
+  groupName(code) {
+    return GROUP[code]
+  },
+  fromForm(data) {
+    return {
+      ...data,
+      id: data.id || `A${Date.now()}`,
+      balance: Object.keys(data.balance).reduce((acc, code) => {
+        acc[code] = Currency.toInt(data.balance[code], code)
+        return acc
+      }, {})
+    }
+  },
+  toForm(data) {
+    return {
+      ...data,
+      balance: Object.keys(data.balance).reduce((acc, code) => {
+        acc[code] = Currency.toFloat(data.balance[code], code)
+        return acc
+      }, {})
+    }
+  },
+  fromStorage(data) {
+    return {
+      id: data._id,
+      ...this.toStorage(data)
+    }
+  },
+  toStorage(data) {
+    return {
+      name: data.name,
+      group: data.group,
+      balance: data.balance
+    }
+  },
+  mutateBalance(account, currency, amount) {
+    return {
+      ...account,
+      balance: {
+        ...account.balance,
+        [currency]: parseInt(account.balance[currency]) + amount
+      }
+    }
+  }
+}
