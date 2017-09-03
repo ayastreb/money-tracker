d087c76b596ff589f3410bd72f3af0efca1cd8a0
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-09-03 15:48:01 +0900

    Style fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux'
 import entities from './entities'
 import ui from './ui'
 import settings from './settings'
-import user from './user'
 import sync from './sync'
+import user from './user'
 
 export default combineReducers({
   entities,
   ui,
   settings,
-  user,
-  sync
+  sync,
+  user
 })
