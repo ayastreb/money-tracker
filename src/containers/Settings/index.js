a42a7728d66ffb43f6fb6c87063992c8091b291d
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-30 13:29:44 -0800

    Style fixes

--- src/containers/Settings/index.js
+++ src/containers/Settings/index.js
@@ -5,14 +5,14 @@ import User from './User'
 import CollapsibleSection from '../../components/CollapsibleSection'
 
 const Settings = () => (
-  <div className="container-full-page">
+  <div className="container-full-page mt-settings">
     <CollapsibleSection name="settings_currency" label="Currency">
       <CurrencySettings />
     </CollapsibleSection>
     <CollapsibleSection name="settings_import" label="Data Import">
       <DataImport />
     </CollapsibleSection>
-    <CollapsibleSection name="settings_user" label="User Settings">
+    <CollapsibleSection name="settings_user" label="User">
       <User />
     </CollapsibleSection>
   </div>
