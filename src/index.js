7b91520d3975108d9306caac91fbc1f265326e8b
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2018-04-25 10:36:49 +0900

    Remove hash history

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
+import { createBrowserHistory } from 'history'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Root from './containers/Root'
-import configureStore from './store/configureStore'
-import configureHistory from './router/configureHistory'
-import registerServiceWorker from './registerServiceWorker'
 import 'semantic-ui-css/semantic.min.css'
+import Root from './containers/Root'
 import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+import configureStore from './store/configureStore'
 
 const store = configureStore()
-const history = configureHistory()
+const history = createBrowserHistory()
 ReactDOM.render(
   <Root store={store} history={history} />,
   document.getElementById('root')
