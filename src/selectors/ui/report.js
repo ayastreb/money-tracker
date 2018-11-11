d7141de64964acbb1118ad42d32a35f60fcca4f7
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2018-11-11 21:24:01 +0900

    NOBUG: fix formatting

--- src/selectors/ui/report.js
+++ src/selectors/ui/report.js
@@ -2,10 +2,7 @@ import Report from '../../entities/Report'
 
 export const getReport = state => state.ui.report
 export const getTimespanLabel = state => {
-  let startdate = new Date(state.ui.report.date.start);
-  startdate.setDate(startdate.getDate()+1);
-  return Report.timespanLabel(
-    startdate,
-    state.ui.report.timespan
-  )
+  const startDate = new Date(state.ui.report.date.start)
+  startDate.setDate(startDate.getDate() + 1)
+  return Report.timespanLabel(startDate, state.ui.report.timespan)
 }
