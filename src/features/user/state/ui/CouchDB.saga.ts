6804d79622c0526fc9e13ee01dd2b3b83d5b5b0e
Chen Yufei <cyfdecyf@gmail.com>
2022-01-09 13:47:29 +0800

    Use common prefix for couchdb database names.

--- src/features/user/state/ui/CouchDB.saga.ts
+++ src/features/user/state/ui/CouchDB.saga.ts
@@ -8,13 +8,14 @@ import { isUserLoggedIn } from '../User.saga';
 
 export function* finishCouchDBSettingSaga() {
   const couchDB = yield select(getCouchDBSetting);
+  const prefix = `moneytracker_${couchDB.username}`;
   const userInfo = {
     accessToken: '',
     couchDB: { ...couchDB,
-      accounts: `${couchDB.url}/accounts_${couchDB.username}`,
-      settings: `${couchDB.url}/settings_${couchDB.username}`,
-      tags: `${couchDB.url}/tags_${couchDB.username}`,
-      transactions: `${couchDB.url}/transactions_${couchDB.username}`,
+      accounts: `${couchDB.url}/${prefix}_accounts`,
+      settings: `${couchDB.url}/${prefix}_settings`,
+      tags: `${couchDB.url}/${prefix}_tags`,
+      transactions: `${couchDB.url}/${prefix}_transactions`,
     }
   }
   yield call([localStorage, 'setItem'], 'userInfo', JSON.stringify(userInfo));
